5a12f017a7a2a7cfb3231092a9f76113c6070c87
speek254well2-creator <speek254well2@gmail.com>
2026-04-09 16:03:09 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,10 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig(({mode}) => {
+  base: '/harrylovekobebean/'
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
